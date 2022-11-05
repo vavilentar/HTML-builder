4144d27ef6dafde8513a1d8a36fd166031984ed4
Roman Pchelkin <48628619+vavilentar@users.noreply.github.com>
2022-11-05 08:35:50 +0300

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const source = './04-copy-directory/files'
+const copy = './04-copy-directory/files-copy'
 
+fs.mkdir(copy, () => {
+	console.log('Make directory done!')
+})
+
+fs.readdir(source, (error, files) => {
+	if (error) throw error;
+	files.forEach(file => {
+		fs.copyFile(`${source}/${file}`, `${copy}/${file}`, () => {})
+	})
+	console.log('Copying done!')
+})
